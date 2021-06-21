dfc3f680da6a838ff28aedd9cb69b78055d8ba59
“[Sandesh <075bct075.sandesh@pcampus.edu.np>
2021-06-21 12:54:39 +0545

    update

--- fish/main.js
+++ fish/main.js
@@ -9,39 +9,53 @@ let h;
 let camX;
 let camY;
 
+// let myFont;
+// function preload() {
+//     myFont = loadFont("Roboto-Regular.ttf");
+// }
+
 function setup() {
     createCanvas(innerWidth, innerHeight, WEBGL);
     
-    // camX = -asin(1/sqrt(3));
-    // camX = -PI/4;
-    // camY = 0;
+    // createLoop({duration:3, gif:"download"})
 
-    camX = 0.63;
+    //camX = -asin(1/sqrt(3));
+    //camY = -PI/4;
+
+	camX = 0.63;
     camY = -0.83;
-
-    leftBound = -width/2 + 500;
+    
+    leftBound = -width/2 + 500
     rightBound = width/2 - 500;
     topBound = -height/2 + 300;
     bottomBound = height/2;
-
+    
     ortho();
+
+    // textSize(40);
+    // textAlign(CENTER, CENTER);
+    // textFont(myFont)
 }
 
 function draw() {
     background(0);
-    // orbitControl();
+    // if(frameCount % 10 == 0) {
+    //     text('fr : ' +  floor(frameRate()), -width/2 + 70, 0);
+    // }
+
+
+    orbitControl();
     // normalMaterial();
     rotateX(camX);
     rotateY(camY);
 
-    
-
     for(let z = topBound; z < bottomBound; z += seperation) {  
         for(let x = leftBound; x < rightBound; x += seperation) {   
             push(); 
-                h = map(sin(angle + x/70 - z/70), -1, 1, minHeight, maxHeight);
-                fill(x + 200, z - 200, x - 100, 10);
-                noStroke()
+                h = map(sin(angle + x/70 + z/50), -1, 1, minHeight, maxHeight);
+                fill(x + 200, z - 200, x - 100, 255);
+                // fill(100,10)
+                // noStroke();
                 translate(x, 0, z);
                 box(widthOfCube, h, widthOfCube);
             pop();
